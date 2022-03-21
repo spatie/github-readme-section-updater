c13daec414911d34444c4244ab07cdd934b5fa9b
Adriaan <adriaanmarain300@gmail.com>
2022-03-21 13:24:33 +0100

    community health files

--- copy.js
+++ copy.js
@@ -1,6 +1,9 @@
-const newCopy = `
-[<img src="https://github-ads.s3.eu-central-1.amazonaws.com/support-ukraine.svg?t=1" />](https://supportukrainenow.org)
+const newCopy =
+    "If you've found a bug regarding security please mail [security@spatie.be](mailto:security@spatie.be) instead of using the issue tracker.";
 
-`;
+const oldCopies = [
+    "If you've found a bug regarding security please mail [freek@spatie.be](mailto:freek@spatie.be) instead of using the issue tracker.",
+    'If you discover any security related issues, please email freek@spatie.be instead of using the issue tracker.',
+];
 
-module.exports = { newCopy };
+module.exports = { newCopy, oldCopies };
--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const { Octokit } = require('@octokit/rest');
 require('dotenv').config();
-const { newCopy } = require('./copy');
+const { newCopy, oldCopies } = require('./copy');
 
 const octokit = new Octokit({ auth: process.env.GITHUB_API_KEY });
 
 function getAllPublicRepoNames(page = 1) {
     return new Promise((resolve) => {
         octokit.repos.listForOrg({ org: 'spatie', per_page: 100, page }).then(async (response) => {
-            let publicRepos = response.data
-                .filter((repo) => !repo.private)
-                .map((r) => ({ name: r.name, html_url: r.html_url }));
+            let publicRepos = response.data.map((r) => ({ name: r.name, html_url: r.html_url }));
 
             if (response.data.length === 100) {
                 const moreRepos = await getAllPublicRepoNames(page + 1);
@@ -39,18 +37,25 @@ function updateReadme(repoInfo, repoURL) {
 
             let readme = Buffer.from(response.data.content, 'base64').toString();
 
-            if (readme.includes(newCopy)) {
-                readme = readme.replace(newCopy, '');
-            } else {
+            let hasCopy = false;
+
+            oldCopies.forEach((oldCopy) => {
+                if (readme.includes(oldCopy)) {
+                    readme = readme.replace(oldCopy, newCopy);
+                    hasCopy = true;
+                }
+            });
+
+            if (!hasCopy) {
                 console.log('skipped', repoURL, 'because it did not have the copy.');
-                return;
+                return resolve();
             }
 
             try {
                 await octokit.repos.createOrUpdateFile({
                     ...repoInfo,
                     path: filename,
-                    message: 'Remove banner',
+                    message: 'Change copy',
                     content: Buffer.from(readme).toString('base64'),
                     sha: response.data.sha,
                 });
@@ -74,6 +79,74 @@ function updateReadme(repoInfo, repoURL) {
     });
 }
 
+const filesToRemove = [
+    'SECURITY.md',
+    'CONTRIBUTING.md',
+    'FUNDING.md',
+    '.github/SECURITY.md',
+    '.github/CONTRIBUTING.md',
+    '.github/FUNDING.md',
+];
+
+function removeAllFiles(repoInfo, repoURL) {
+    return new Promise(async (resolve) => {
+        try {
+            filesToRemove.reduce((prev, currFile) => {
+                return new Promise(async (resolve) => {
+                    await prev;
+                    await removeFile(repoInfo, currFile);
+                    resolve();
+                });
+            }, Promise.resolve());
+        } catch (error) {
+            console.log('something went wrong while updating', repoInfo.repo, repoURL);
+            console.log(error);
+        }
+
+        resolve();
+    });
+}
+
+function removeFile(repoInfo, path) {
+    return new Promise(async (resolve) => {
+        try {
+            let response;
+
+            try {
+                response = await octokit.repos.getContents({ ...repoInfo, path });
+            } catch (error) {
+                if (error.message.includes('Not Found')) {
+                    return resolve();
+                } else {
+                    throw error;
+                }
+            }
+
+            try {
+                await octokit.repos.deleteFile({
+                    ...repoInfo,
+                    path,
+                    message: 'Use organisation-wide community health files',
+                    sha: response.data.sha,
+                });
+            } catch (error) {
+                if (error.message.includes('archived')) {
+                    // noop
+                } else if (error.message.includes('repository is empty')) {
+                    // noop
+                } else {
+                    throw error;
+                }
+            }
+        } catch (error) {
+            console.log('something went wrong while updating', repoInfo.repo);
+            console.log(error);
+        }
+
+        resolve();
+    });
+}
+
 let runAll = false;
 function askUserInput(nextRepoName) {
     return new Promise((resolve) => {
@@ -81,7 +154,9 @@ function askUserInput(nextRepoName) {
             return resolve();
         }
 
-        console.log(`\nPress "a" to edit all repos, press "c" to exit, press any other key to step to the next repo (${nextRepoName})\n`);
+        console.log(
+            `\nPress "a" to edit all repos, press "c" to exit, press any other key to step to the next repo (${nextRepoName})\n`
+        );
 
         process.stdin.setRawMode(true);
         process.stdin.resume();
@@ -112,7 +187,8 @@ getAllPublicRepoNames().then((allRepos) => {
 
             console.log('updating', currRepo.name, currRepo.html_url);
 
-            await updateReadme({ owner: 'spatie', repo: currRepo.name }, currRepo.html_url);
+            /* await updateReadme({ owner: 'spatie', repo: currRepo.name }, currRepo.html_url); */
+            await removeAllFiles({ owner: 'spatie', repo: currRepo.name }, currRepo.html_url);
 
             resolve();
         });
